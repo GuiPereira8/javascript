65ab4266f4d92b91c1eb003dabfc5a4527da6aa0
Guilherme Pereira <guiipereiraarts@gmail.com>
2025-09-09 15:52:49 -0300

    Aula 13 Estrutura de repetição

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,21 @@
+let contador = 1
+do {
+    console.log(`${contador} ` + 'Olá, Mundo!');
+    contador++
+} while (contador <= 5)
+
+/*
+let contador = 1
+while (contador <= 5) {
+    console.log(`${contador} ` + 'Olá, Mundo!');
+    contador++
+}
+*/
+
+/*
+console.log('Olá, Mundo!');
+console.log('Olá, Mundo!');
+console.log('Olá, Mundo!');
+console.log('Olá, Mundo!');
+console.log('Olá, Mundo!');
+*/
